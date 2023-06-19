ad2fc010f38dac6df3c0a90e95fa9a401342bacb
ashutshm778 <ashutoshm778@gmail.com>
2023-06-19 16:56:45 +0530

    new update

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -150,8 +150,9 @@
     <div class="container">
         <div class="ec-nav-panel">
             <div class="ec-nav-panel-icons">
-                <a href="#" onclick="copy_text()" class="navbar-toggler-btn ec-header-btn ec-side-toggle">
-                    <i class="ecicon eci-copy"></i>
+                <a href="#ec-mobile-menu" class="navbar-toggler-btn ec-header-btn ec-side-toggle">
+                    <img src="{{ asset('public/frontend/assets/images/icons/menu.svg') }}" class="svg_img header_svg"
+                        alt="" />
                 </a>
             </div>
             <div class="ec-nav-panel-icons">
@@ -634,12 +635,4 @@
         });
     }
 
-function copy_text(){
-    var text = '';
-    navigator.clipboard.writeText(text).then(function() {
-  console.log('Async: Copying to clipboard was successful!');
-}, function(err) {
-  console.error('Async: Could not copy text: ', err);
-});
-}
 </script>
