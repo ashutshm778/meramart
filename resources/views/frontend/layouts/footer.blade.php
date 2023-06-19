07d87bb013ada995cea4107b646f21bab1744596
ashutshm778 <ashutoshm778@gmail.com>
2023-06-19 18:50:42 +0530

    new update

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -643,15 +643,65 @@
      @if(Auth::guard('customer')->user()->verify_status==1)
         function copy_text() {
             var text = "https://themeramart.com/user-register-mlm?referral_code={{ Auth::guard('customer')->user()->referral_code }}";
-            navigator.clipboard.writeText(text);
+            CopyToClipBoard(text);
         }
+
+
+
     @endif
     @endif
-    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
-  if (result.state == "granted" || result.state == "prompt") {
-    alert("Write access granted!");
-  }else{
-    alert("Write access not granted!");
-  }
-});
+
+
+    function CopyToClipBoard(textToCopy) {
+var successMessage = 'Success! The text was copied to your clipboard';
+var errorMessage = 'Oops! Copy to clipboard failed. ';
+
+// navigator clipboard api needs a secure context (https)
+if (navigator.clipboard && window.isSecureContext) {
+
+    // navigator clipboard api method'
+    navigator.clipboard.writeText(textToCopy).then(
+        function () {
+            /* clipboard successfully set */
+            console.log(successMessage)
+        },
+        function () {
+            /* clipboard write failed */
+            console.warn(errorMessage)
+        }
+    )
+} else
+    if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+
+    // text area method
+    var textarea = document.createElement("textarea");
+    textarea.value = textarea.textContent = textToCopy;
+    textarea.style.opacity = "0";
+
+    document.body.appendChild(textarea);
+    textarea.focus();
+        textarea.select();
+
+        var selection = document.getSelection();
+        var range = document.createRange();
+
+        range.selectNode(textarea);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+    try {
+        var successful = document.execCommand('copy'); // Security exception may be thrown by some browsers.
+        var msg = successful ? console.log(successMessage) : console.warn(errorMessage);
+    }
+    catch (ex) {
+        console.warn(errorMessage, ex);
+    }
+    finally {
+        selection.removeAllRanges();
+        document.body.removeChild(textarea);
+    }
+}
+}
+
+
 </script>
