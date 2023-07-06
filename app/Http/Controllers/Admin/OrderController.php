2095a65505bceff8e6f1389ba8f3368ad7f88aae
ashutshm778 <ashutoshm778@gmail.com>
2023-07-06 12:51:22 +0530

    new update

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -16,41 +16,43 @@ use Illuminate\Support\Facades\Auth;
 class OrderController extends Controller
 {
 
-    public function index(Request $request){
-        $orders = Order::withCount('order_details')->orderBy('id','desc')->paginate(10);
+    public function index(Request $request)
+    {
+        $orders = Order::withCount('order_details')->orderBy('id', 'desc')->paginate(10);
 
-        return view('backend.order.index',compact('orders'),['page_title'=>'Order List']);
+        return view('backend.order.index', compact('orders'), ['page_title' => 'Order List']);
     }
 
-    public function detail($order_id){
-        $order = Order::where('order_id',$order_id)->with(['order_details','customer'])->first();
+    public function detail($order_id)
+    {
+        $order = Order::where('order_id', $order_id)->with(['order_details', 'customer'])->first();
 
-        return view('backend.order.detail',compact('order'),['page_title'=>'Order Details']);
+        return view('backend.order.detail', compact('order'), ['page_title' => 'Order Details']);
     }
 
-    public function productStatus(Request $request){
+    public function productStatus(Request $request)
+    {
 
-        if(!is_array($request->product_id)){
-            $product_ids = explode(',',$request->product_id);
-        }else{
+        if (!is_array($request->product_id)) {
+            $product_ids = explode(',', $request->product_id);
+        } else {
             $product_ids = $request->product_id;
         }
         foreach ($product_ids as $product_id) {
-            OrderDetail::where('order_id',$request->order_id)->where('product_id',$product_id)->update([
-                'order_status'=>$request->status
+            OrderDetail::where('order_id', $request->order_id)->where('product_id', $product_id)->update([
+                'order_status' => $request->status
             ]);
         }
 
-        Order::where('id',$request->order_id)->update([
-            'order_status'=>$request->status
+        Order::where('id', $request->order_id)->update([
+            'order_status' => $request->status
         ]);
-
-
     }
 
-    public function paymentStatus(Request $request){
+    public function paymentStatus(Request $request)
+    {
 
-        $order=Order::find($request->id);
+        $order = Order::find($request->id);
         $order->payment_status = $request->payment_status;
         $order->remark = $request->remark;
         $order->save();
@@ -76,50 +78,48 @@ class OrderController extends Controller
                     for ($i = 1; $i <= $level; $i++) {
                         $refferal_customer = Customer::where('referral_code', $referral_code)->first();
 
-                        if (Customer::where('refered_by', $referral_code)->where('verify_status',1)->get()->count() % 2 == 0) {
+                        if (Customer::where('refered_by', $referral_code)->where('verify_status', 1)->get()->count() % 2 == 0) {
 
-                                $all_user_ids=Customer::where('refered_by', $referral_code)->where('verify_status',1)->get()->pluck('id')->toArray();
-                                $all_commission_direct_user_id=CommissionDirect::where('user_id',$refferal_customer->id)->where('direct_type',2)->get()->pluck('direct_user_id')->toArray();
-
-                                $diff = array_diff($all_user_ids, $all_commission_direct_user_id);
-
-                                foreach($diff as $userId){
-
-                                    $commission_direct = new CommissionDirect;
-                                    $commission_direct->user_id = $refferal_customer->id;
-                                    $commission_direct->order_id = $order->id;
-                                    $commission_direct->commission = 300;
-                                    $commission_direct->direct_type = 2;
-                                    $commission_direct->direct_user_id = $userId;
-                                    $commission_direct->save();
-
-                                }
-
-
-                                $refferal_customer->balance = $refferal_customer->balance + 600;
-                                $refferal_customer->save();
-
-                                $customer_wallet = new CustomerWallet;
-                                $customer_wallet->user_id = $refferal_customer->id;
-                                $customer_wallet->amount = 600;
-                                $customer_wallet->transaction_type = 'credited';
-                                $customer_wallet->transaction_detail = 'Comission Credited For Two Direct';
-                                $customer_wallet->payment_details = '';
-                                $customer_wallet->balance = $refferal_customer->balance;
-                                $customer_wallet->approval = 0;
-                                $customer_wallet->save();
-
-                        }
-
-                        if (Customer::where('refered_by', $referral_code)->get()->where('verify_status',1)->count() % 10 == 0) {
-
-
-                            $all_user_ids=Customer::where('refered_by', $referral_code)->where('verify_status',1)->get()->pluck('id')->toArray();
-                            $all_commission_direct_user_id=CommissionDirect::where('user_id',$refferal_customer->id)->where('direct_type',10)->get()->pluck('direct_user_id')->toArray();
+                            $all_user_ids = Customer::where('refered_by', $referral_code)->where('verify_status', 1)->get()->pluck('id')->toArray();
+                            $all_commission_direct_user_id = CommissionDirect::where('user_id', $refferal_customer->id)->where('direct_type', 2)->get()->pluck('direct_user_id')->toArray();
 
                             $diff = array_diff($all_user_ids, $all_commission_direct_user_id);
 
-                            foreach($diff as $userId){
+                            foreach ($diff as $userId) {
+
+                                $commission_direct = new CommissionDirect;
+                                $commission_direct->user_id = $refferal_customer->id;
+                                $commission_direct->order_id = $order->id;
+                                $commission_direct->commission = 300;
+                                $commission_direct->direct_type = 2;
+                                $commission_direct->direct_user_id = $userId;
+                                $commission_direct->save();
+                            }
+
+
+                            $refferal_customer->balance = $refferal_customer->balance + 600;
+                            $refferal_customer->save();
+
+                            $customer_wallet = new CustomerWallet;
+                            $customer_wallet->user_id = $refferal_customer->id;
+                            $customer_wallet->amount = 600;
+                            $customer_wallet->transaction_type = 'credited';
+                            $customer_wallet->transaction_detail = 'Comission Credited For Two Direct';
+                            $customer_wallet->payment_details = '';
+                            $customer_wallet->balance = $refferal_customer->balance;
+                            $customer_wallet->approval = 0;
+                            $customer_wallet->save();
+                        }
+
+                        if (Customer::where('refered_by', $referral_code)->get()->where('verify_status', 1)->count() % 10 == 0) {
+
+
+                            $all_user_ids = Customer::where('refered_by', $referral_code)->where('verify_status', 1)->get()->pluck('id')->toArray();
+                            $all_commission_direct_user_id = CommissionDirect::where('user_id', $refferal_customer->id)->where('direct_type', 10)->get()->pluck('direct_user_id')->toArray();
+
+                            $diff = array_diff($all_user_ids, $all_commission_direct_user_id);
+
+                            foreach ($diff as $userId) {
 
                                 $commission_direct = new CommissionDirect;
                                 $commission_direct->user_id = $refferal_customer->id;
@@ -128,23 +128,21 @@ class OrderController extends Controller
                                 $commission_direct->direct_type = 10;
                                 $commission_direct->direct_user_id = $userId;
                                 $commission_direct->save();
-
                             }
 
 
-                                $refferal_customer->balance = $refferal_customer->balance + 2150;
-                                $refferal_customer->save();
+                            $refferal_customer->balance = $refferal_customer->balance + 2150;
+                            $refferal_customer->save();
 
-                                $customer_wallet = new CustomerWallet;
-                                $customer_wallet->user_id = $refferal_customer->id;
-                                $customer_wallet->amount = 2560 ;
-                                $customer_wallet->transaction_type = 'credited';
-                                $customer_wallet->transaction_detail = 'Comission Credited For Ten Direct';
-                                $customer_wallet->payment_details = '';
-                                $customer_wallet->balance = $refferal_customer->balance;
-                                $customer_wallet->approval = 0;
-                                $customer_wallet->save();
-
+                            $customer_wallet = new CustomerWallet;
+                            $customer_wallet->user_id = $refferal_customer->id;
+                            $customer_wallet->amount = 2560;
+                            $customer_wallet->transaction_type = 'credited';
+                            $customer_wallet->transaction_detail = 'Comission Credited For Ten Direct';
+                            $customer_wallet->payment_details = '';
+                            $customer_wallet->balance = $refferal_customer->balance;
+                            $customer_wallet->approval = 0;
+                            $customer_wallet->save();
                         }
 
                         if (!empty($refferal_customer->id)) {
@@ -225,8 +223,5 @@ class OrderController extends Controller
         }
 
         return redirect()->back();
-
-     }
-
-
+    }
 }
