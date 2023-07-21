7329557724b98470e3ebff1c52aa41c36758d3b3
ashutshm778 <ashutoshm778@gmail.com>
2023-07-21 15:56:59 +0530

    new update

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -374,7 +374,7 @@ class FrontController extends Controller
         if (!$children->count()) {
             return [
                 'v' => $node->referral_code ?: $node->id,
-                'f' =>'<div class=mytooltip><img src='.($node->verify_status == 1 ? asset('/public/green.png') : asset('/public/red.png')).' style=height:50px;width:50px;><a
+                'f' =>'<div class=mytooltip><img src='.($node->verify_status == 1 ? (asset('public/public/frontend/user_profile/'.$node->photo)?: asset('/public/green.png')) : (asset('public/public/frontend/user_profile/'.$node->photo)?: asset('/public/red.png'))).' style=height:50px;width:50px;><a
                 href='.route('tree_view').'?referral_code='.$node->referral_code.'><span style=color:black>'.$node->referral_code.'</span><br><span
                      style=color:black>'.$node->first_name.'</span></a><span class=mytext id=my'.$node->referral_code.'></span></div>' ,
                 'p' => $node->refered_by ?: null,
@@ -382,7 +382,7 @@ class FrontController extends Controller
         }
         $tree = [
             'v' => $node->referral_code ?: $node->id,
-            'f' => '<div class=mytooltip><img src='.($node->verify_status == 1 ? asset('/public/green.png'): asset('/public/red.png')).' style=height:50px;width:50px;><a
+            'f' => '<div class=mytooltip><img src='.($node->verify_status == 1 ? (asset('public/public/frontend/user_profile/'.$node->photo)?: asset('/public/green.png')): (asset('public/public/frontend/user_profile/'.$node->photo)?: asset('/public/red.png'))).' style=height:50px;width:50px;><a
             href='.route('tree_view').'?referral_code='.$node->referral_code.'><span style=color:black>'.$node->referral_code.'</span><br><span
                  style=color:black>'.$node->first_name.'</span></a><span class=mytext id=my'.$node->referral_code.'></span></div>',
             'p' => $node->refered_by ?: null,
