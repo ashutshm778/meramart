1bc62f81cdc684d0aa15058cdfb112910bac5d3c
ashutshm778 <ashutoshm778@gmail.com>
2023-10-20 15:27:10 +0530

    new update

--- app/Http/Controllers/LevelIncomeController.php
+++ app/Http/Controllers/LevelIncomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Auth;
+use App\Models\Order;
 use App\Models\Commission;
 use Illuminate\Http\Request;
 
@@ -15,6 +16,18 @@ class LevelIncomeController extends Controller
         $final_arr = [];
         foreach($levels as $key=>$level){
             $teams = Commission::where('user_id',Auth::guard('customer')->user()->id)->where('level',$level)->get();
+
+            $total_pv_all=0;
+           foreach($teams as $team){
+             $order_data=Order::where('user_id', $team->order->user_id)->where('payment_status','success')->get();
+             $total_pv=0;
+             foreach($order_data as $data){
+              foreach($data->order_details as $order_detail){
+               $total_pv= $total_pv + ($order_detail->pv *  $order_detail->quantity);
+               }
+              }
+             }
+
             $total_team = $teams->count();
             $my_income = $teams->sum('commission');
             array_push($final_arr,['level'=>$level,'income'=>$commissions[$key],'total_team'=>$total_team,'my_income'=>$my_income]);
