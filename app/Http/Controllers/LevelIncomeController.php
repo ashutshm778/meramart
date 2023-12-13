d51188296e56f8fb9fc2b049d49a00fdcd2e34b6
ashutshm778 <ashutoshm778@gmail.com>
2023-12-13 18:06:14 +0530

    new update

--- app/Http/Controllers/LevelIncomeController.php
+++ app/Http/Controllers/LevelIncomeController.php
@@ -69,19 +69,12 @@ class LevelIncomeController extends Controller
         $children = $this->getChildren($referralCode);
         if (!$children->count()) {
             return [
-                'v' => $node->referral_code,
-                'f' =>'<div class=mytooltip><img src='.($node->verify_status == 1 ? ($node->photo ? asset('public/public/frontend/user_profile/'.$node->photo): asset('/public/green.png')) : ($node->photo ?asset('public/public/frontend/user_profile/'.$node->photo) : asset('/public/red.png'))).' style=height:50px;width:50px;><a
-                href='.route('tree_view').'?referral_code='.$node->referral_code.'><span style=color:black>'.$node->referral_code.'</span><br><span
-                     style=color:black>'.$node->name.'</span></a><span class=mytext id=my'.$node->referral_code.'></span></div>' ,
-                'p' => $node->refered_by ?: null,
+                'user_id' => $node->id,
+
             ];
         }
         $tree = [
-            'v' => $node->referral_code,
-            'f' => '<div class=mytooltip><img src='.($node->verify_status == 1 ? ($node->photo ? asset('public/public/frontend/user_profile/'.$node->photo): asset('/public/green.png')): ( $node->photo ? asset('public/public/frontend/user_profile/'.$node->photo) : asset('/public/red.png'))).' style=height:50px;width:50px;><a
-            href='.route('tree_view').'?referral_code='.$node->referral_code.'><span style=color:black>'.$node->referral_code.'</span><br><span
-                 style=color:black>'.$node->name.'</span></a><span class=mytext id=my'.$node->referral_code.'></span></div>',
-            'p' => $node->refered_by ?: null,
+            'user_id' => $node->id,
             'c' => [],
         ];
         foreach ($children as $child) {
